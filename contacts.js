fd1261d5ffe83e198fea49e24ca15fb45a3405d1
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-06-14 17:23:17 +0300

    add contacts

---
+++ contacts.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+// import fs from 'fs';
+// import path from 'path';
+
+const contactsPath = require("./db/contacts.json");
+console.log(contactsPath);
+
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
