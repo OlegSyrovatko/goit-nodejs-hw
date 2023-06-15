150f53512d4685eb1d9210f8b25f157375e37244
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-06-15 12:15:54 +0300

    add contacts functions

--- contacts.js
+++ contacts.js
@@ -1,25 +1,77 @@
 
-
-const contacts = require('./db/contacts.json');
 const fs = require('fs').promises;
+const path = require('path');
 
+// const contacts = require('./db/contacts.json');
+//     console.log(contacts);
+// const contactsPath = "./db/contacts.json";
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+
+function generateRandomId(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const contacts = JSON.parse(data);
     console.log(contacts);
-
-
-
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((c) => c.id === contactId);
+    console.log(contact);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  try {
+    let data = await fs.readFile(contactsPath, 'utf8');
+    let contacts = JSON.parse(data);
+    contacts = contacts.filter((c) => c.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts), 'utf8');
+    console.log('Contact removed successfully');
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, phone) {
+  try {
+    let data = await fs.readFile(contactsPath, 'utf8');
+    let contacts = JSON.parse(data);
+    const newContact = {
+      id: generateRandomId(20),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts), 'utf8');
+    console.log('Contact added successfully');
+  } catch (err) {
+    console.error(err);
+  }
 }
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 console.log("Wellcome");
 
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require('./contacts.js');
+
+
+
+listContacts();
+
+// Get contact by ID
+// getContactById('10');
+
+// Remove a contact
+// removeContact('10');
+
+// Add a new contact
+// addContact('Oleg', 'oleg@example.com', '(123) 999-9999');
